7772efe495c63fed9c389b29136baabb17832af3
Vincenzo Ciaccio <vincenzo.ciaccio@gmail.com>
2020-04-05 15:55:24 +0100

    adding and testing button

---
+++ src/components/common/Button.js
@@ -0,0 +1,22 @@
+import React from 'react';
+
+const Button = ({ label, className = '' }) => {
+    return (
+        <button className={
+        `
+        bg-blue-500
+        hover:bg-blue-400
+        text-white
+        font-bold 
+        py-2 px-4 border-b-4 
+        border-blue-700
+        hover:border-blue-500 
+        rounded ${className}`
+        }
+        >
+            {label}
+        </button>
+    );
+};
+
+export default Button;
--- src/components/ui/CommandBar.js
+++ src/components/ui/CommandBar.js
@@ -1,5 +1,7 @@
 import React from 'react';
 
+import { Button } from 'components/common';
+
 const CommandBar = () => {
     return (
         <div className="
@@ -7,10 +9,11 @@ const CommandBar = () => {
         text-center
         bg-gray-400
         px-4 py-2 m-2 
-        flex-1 flex flex-row 
+        flex-1 flex flex-col 
         items-center justify-center"
         >
-            1
+            <Button label="Some stuff I dont know" />
+            <Button label="Jesus my god I would not if I were you" />
         </div>
     );
 };
