100341f864473978584091ccade866d5abec8820
Vincenzo Ciaccio <vincenzo.ciaccio@gmail.com>
2020-04-13 18:53:19 +0100

    fixed message with icon

---
+++ src/components/common/Icon.js
@@ -0,0 +1,27 @@
+import React from 'react';
+
+const NAMES = {
+    CROSS: 'cross'
+};
+
+const map = {
+    [NAMES.CROSS]: 'M14.348 14.849a1.2 1.2 0 0 1-1.697 0L10 11.819l-2.651 3.029a1.2 1.2 0 1 1-1.697-1.697l2.758-3.15-2.759-3.152a1.2 1.2 0 1 1 1.697-1.697L10 8.183l2.651-3.031a1.2 1.2 0 1 1 1.697 1.697l-2.758 3.152 2.758 3.15a1.2 1.2 0 0 1 0 1.698z'
+}
+
+const Icon = ({ name, className = '', ...others }) => {
+    return (
+        <svg
+            xmlns="http://www.w3.org/2000/svg"
+            className={`fill-current ${className}`}
+            viewBox="0 0 20 20"
+            {...others}
+        >
+            <title>Close</title>
+            <path d={map[name]} />
+        </svg>
+    )
+}
+
+Icon.names = NAMES;
+
+export default Icon;
--- src/components/controls/Message.js
+++ src/components/controls/Message.js
@@ -1,26 +1,21 @@
 import React from 'react';
 import { useStoreon } from 'storeon/react';
 
-import { Button } from 'components/common';
+import { Button, Icon } from 'components/common';
 
 const Message = () => {
     const { dispatch, ui: { message } } = useStoreon('ui');
     return (
-        <div className="flex flex-col min-w-full">
-            <div className="flex flex-row 
-                items-center justify-center 
-                bg-gray-200 shadow w-auto h-auto p-5"
-            >
-                {message}
-            </div>
-            <div className="flex flex-row justify-end">
-                <Button
-                    noPadding
+        <div className="bg-blue-100 border-t border-b border-blue-500 text-blue-700 px-4 py-3 rounded relative min-w-full" role="alert">
+            <span className="block sm:inline">{message}</span>
+            <span className="absolute top-0 bottom-0 right-0 px-4 py-3">
+                <Icon
+                    name={Icon.names.CROSS}
+                    className="h-6 w-6 text-blue-500"
+                    role="button"
                     onClick={() => dispatch('clearMessage')}
-                >
-                    Close
-                </Button>
-            </div>
+                />
+            </span>
         </div>
     )
 };
