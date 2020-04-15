8626b3311a5ef2d19396d1252f982321532b4e1c
Vincenzo Ciaccio <vincenzo.ciaccio@gmail.com>
2020-04-15 22:05:56 +0100

    using icons instead of text

--- src/components/controls/Actions.js
+++ src/components/controls/Actions.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { useStoreon } from 'storeon/react';
 
-import { Button } from 'components/common';
+import { Button, Icon } from 'components/common';
 import { ACTIONS_LABELS } from 'downara';
 import { ACTIONS, areaHelper } from 'lib/game';
 
@@ -9,28 +9,34 @@ const Actions = () => {
     const { dispatch, gameState: { player, actionedTile } } = useStoreon('gameState');
     const { areaPosition: playerAreaPosition } = player;
     const isPlayerTile = areaHelper.isSameTile(actionedTile.position, playerAreaPosition);
+
+
+
     return (
         <div>
             <Button
                 className="flex-1"
-                label={ACTIONS_LABELS[ACTIONS.MOVE]}
-                disabled={isPlayerTile}
-                onClick={() => dispatch('moveToTile', actionedTile.position)}
-            />
+                disabled={!isPlayerTile}
+                onClick={() => dispatch('examine')}
+            >
+                <Icon name={Icon.names.MENU} />
+            </Button>
 
             <Button
                 className="flex-1"
-                label={ACTIONS_LABELS[ACTIONS.EXAMINE]}
                 disabled={!isPlayerTile}
                 onClick={() => dispatch('examine')}
-            />
-            
+            >
+                <Icon name={Icon.names.SEARCH} title={ACTIONS_LABELS[ACTIONS.EXAMINE]} />
+            </Button>
+
             <Button
                 className="flex-1"
-                label={ACTIONS_LABELS[ACTIONS.INTERACT]}
                 disabled={!isPlayerTile}
                 onClick={() => dispatch('interact')}
-            />
+            >
+                <Icon name={Icon.names.BOLT} title={ACTIONS_LABELS[ACTIONS.INTERACT]} />
+            </Button>
         </div>
     );
 };
