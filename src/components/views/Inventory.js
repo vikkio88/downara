764512a7226f566eb20d4acf083fbb033f03da15
Vincenzo Ciaccio <vincenzo.ciaccio@gmail.com>
2020-04-18 10:10:27 +0100

    working on inventory

--- src/components/views/Inventory.js
+++ src/components/views/Inventory.js
@@ -1,12 +1,68 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useStoreon } from 'storeon/react';
 import { Segment } from './common';
 import { LABELS } from 'downara';
+
+const itemFactory = (type, name, description, quantity = 1) => {
+    return {
+        name, description, quantity, type, actions: ITEM_TYPES_ACTIONS[type]
+    };
+};
+const ITEM_TYPES = {
+    WEAPON: 'weapon',
+    WEARABLE: 'wearable',
+    CONSUMABLE: 'consumable',
+    QUEST_ITEM: 'quest_item',
+    USABLE: 'usable'
+};
+
+const ITEM_ACTIONS = {
+    EQUIP: 'equip',
+    DROP: 'drop',
+    USE: 'use',
+    CONSUME: 'consume'
+}
+
+const ITEM_TYPES_ACTIONS = {
+    [ITEM_TYPES.WEAPON]: [ITEM_ACTIONS.EQUIP, ITEM_ACTIONS.DROP],
+    [ITEM_TYPES.WEARABLE]: [ITEM_ACTIONS.EQUIP, ITEM_ACTIONS.DROP],
+    [ITEM_TYPES.CONSUMABLE]: [ITEM_ACTIONS.CONSUME, ITEM_ACTIONS.DROP],
+    [ITEM_TYPES.QUEST_ITEM]: [],
+    [ITEM_TYPES.USABLE]: [ITEM_ACTIONS.USE, ITEM_ACTIONS.DROP],
+};
+
 const items = [
-    { name: 'Knife', description: 'weapon', quantity: 1 },
-    { name: 'Bread', description: 'mezzo chilo di pane, due panini vuoti', quantity: 1 },
-    { name: 'Health Potion', description: '+50 Health', quantity: 4 },
+    itemFactory(ITEM_TYPES.WEAPON, 'Cutidduzzu', 'un piccolo coltellino svizzero'),
+    itemFactory(ITEM_TYPES.WEARABLE, 'Elmetto Bauli®', 'un elmetto ottenuto dal cartone del panettone Bauli®'),
+    itemFactory(ITEM_TYPES.QUEST_ITEM, 'Pane', '½ Kg di pane e due panini vuoti'),
+    itemFactory(ITEM_TYPES.CONSUMABLE, 'Goleador alla coca-cola', 'Rimedio contro qualsiasi ferita', 4),
+    itemFactory(ITEM_TYPES.CONSUMABLE, 'Cerotto', 'Cura +1', 10),
+
 ];
+
+const Item = ({ quantity, name, description }) => {
+    const [toggle, setToggle] = useState(false);
+    return (
+        <Segment onClick={() => setToggle(!toggle)}>
+            <div className="flex-1 flex flex-row justify-between">
+                <div>
+                    {quantity}
+                </div>
+                <div>
+                    {name}
+                </div>
+                <div>
+                    Actions
+                </div>
+            </div>
+            {toggle && (
+                <div>
+                    {description}
+                </div>
+            )}
+        </Segment>
+    );
+};
 const Inventory = () => {
     const { gameState: { inventory } } = useStoreon('gameState');
     const { money } = inventory;
@@ -20,19 +76,7 @@ const Inventory = () => {
                     {money} {LABELS.CURRENCY_SYMBOL}
                 </Segment>
             </Segment>
-            {items.map(({ quantity, name, description }) => (
-                <Segment row center>
-                    <Segment style={{ flex: 1 }} className="text-center">
-                        {quantity}
-                    </Segment>
-                    <Segment style={{ flex: 3 }} row>
-                        {name} - {description}
-                    </Segment>
-                    <Segment style={{ flex: 1 }} row>
-                        Actions
-                        </Segment>
-                </Segment>
-            ))}
+            {items.map((item, i) => (<Item key={`item-row-${i}`} {...item} />))}
         </>
     )
 };
--- src/components/views/common/Segment.js
+++ src/components/views/common/Segment.js
@@ -1,13 +1,14 @@
 import React from 'react';
 
 
-const Segment = ({ children, className = '', center = false, style = null, row = false }) => {
+const Segment = ({ children, className = '', center = false, row = false, spaced = true, ...others }) => {
     const orientation = row ? 'row' : 'col';
     const centering = center ? 'center' : 'between';
+    const spacing = spaced ? 'm4' : '';
     return (
         <div
-            className={`border border-gray-400 bg-white rounded-b p-4 m-4 flex flex-${orientation} justify-${centering} ${className}`}
-            style={style}
+            className={`border border-gray-400 bg-white rounded-b p-4 ${spacing} flex flex-${orientation} justify-${centering} ${className}`}
+            {...others}
         >
             {children}
         </div>
