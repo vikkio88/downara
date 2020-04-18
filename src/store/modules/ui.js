57e3c8027ed9cc8b824f04dacc09629d6f820958
Vincenzo Ciaccio <vincenzo.ciaccio@gmail.com>
2020-04-18 10:11:13 +0100

    removed test

--- src/store/modules/ui.js
+++ src/store/modules/ui.js
@@ -7,8 +7,8 @@ export default store => {
             ui: {
                 message: null,
                 // Temp to test
-                view: VIEWS.INVENTORY,
-                //view: null,
+                //view: VIEWS.INVENTORY,
+                view: null,
                 notification: null
             }
         };
