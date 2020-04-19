8a6e57cf0f82e4cd35cb366f874abd77bee8d98c
Vincenzo Ciaccio <vincenzo.ciaccio@gmail.com>
2020-04-19 18:17:43 +0100

    adding more areas

--- src/downara/areas/index.js
+++ src/downara/areas/index.js
@@ -1,8 +1,10 @@
 import area_0 from './0.json';
+import area_1 from './1.json';
 import area_4 from './4.json';
 
 
 export default {
     0: area_0,
+    1: area_1,
     4: area_4,
 };
--- src/downara/map.js
+++ src/downara/map.js
@@ -10,36 +10,30 @@ export default {
     0: {
         id: 0,
         label: 'Madonna di Trapani',
+        1: START_POINTS[DIRECTIONS.NORTH],
+        2: START_POINTS[DIRECTIONS.SOUTH],
+        3: START_POINTS[DIRECTIONS.WEST],
         4: START_POINTS[DIRECTIONS.EAST],
+    },
+    1: {
+        id: 1,
+        label: 'Porta Nuova',
+        0: START_POINTS[DIRECTIONS.SOUTH]
+    },
+    2: {
+        id: 2,
+        label: 'Via San Francesco',
+        0: START_POINTS[DIRECTIONS.NORTH]
+    },
+    3: {
+        id: 3,
+        label: 'Contrada Luni',
+        0: START_POINTS[DIRECTIONS.WEST]
     },
     4: {
         id: 4,
         label: 'La Matrice',
         0: START_POINTS[DIRECTIONS.WEST],
-    },
-    1: {
-        id: 1,
-        label: 'Porta Nuova',
-        [DIRECTIONS.NORTH]: null,
-        [DIRECTIONS.SOUTH]: 0,
-        [DIRECTIONS.EAST]: null,
-        [DIRECTIONS.WEST]: null,
-    },
-    2: {
-        id: 2,
-        label: 'Via San Francesco',
-        [DIRECTIONS.NORTH]: 0,
-        [DIRECTIONS.SOUTH]: 6,
-        [DIRECTIONS.EAST]: null,
-        [DIRECTIONS.WEST]: null,
-    },
-    3: {
-        id: 3,
-        label: 'Contrada Luni',
-        [DIRECTIONS.NORTH]: null,
-        [DIRECTIONS.SOUTH]: null,
-        [DIRECTIONS.EAST]: null,
-        [DIRECTIONS.WEST]: 0,
     },
     5: {
         id: 5,
