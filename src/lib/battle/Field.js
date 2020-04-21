f91d20d436cd03d2e738a8c89e8bd6dbacf7bd34
Vincenzo Ciaccio <vincenzo.ciaccio@gmail.com>
2020-04-21 20:11:41 +0100

    small functions adjustement

--- src/lib/battle/Field.js
+++ src/lib/battle/Field.js
@@ -1,29 +1,46 @@
 import get from 'lodash.get';
 import { range } from 'lib';
-const tileGenerator = ({ terrain, blocked = false }) => {
-    return { terrain, blocked };
+
+const tileGenerator = (customTile, defaultTile) => {
+    return {
+        ...defaultTile,
+        ...customTile
+    };
 };
-const defaultConfig = {
-    tile: {
+
+export const defaultConfig = {
+    size: 4,
+    defaultTile: {
         terrain: 'd',
         blocked: false,
     }
 };
 
 export class Field {
-    constructor(size = 4, config = defaultConfig, tiles = {}) {
-        this.size = size;
+    constructor(config = {}, tiles = {}) {
+        config = {
+            ...defaultConfig,
+            ...config,
+        };
+        this.defaultTile = config.defaultTile;
+        this.size = config.size;
         this.tiles = [];
-        for (const i in range(0, size)) {
+        for (const i in range(0, this.size)) {
             this.tiles[i] = [];
-            for (const j in range(0, size)) {
+            for (const j in range(0, this.size)) {
                 const customTile = get(tiles, `${i}.${j}`, null);
-                this.tiles[i][j] = customTile ? tileGenerator(customTile) : defaultConfig;
+                this.tiles[i][j] = customTile ?
+                    tileGenerator(customTile, this.defaultTile)
+                    : this.defaultTile;
             }
         }
     }
 
     getTile(i, j) {
         return get(this.tiles, `${i}.${j}`, null);
+    }
+
+    isAdjacent({ i: pi, j: pj }, { i: p1i, j: p1j }, maxD = 1) {
+        return Math.abs(pi - p1i) <= maxD && Math.abs(pj - p1j) <= maxD;
     }
 }
--- src/lib/battle/Field.test.js
+++ src/lib/battle/Field.test.js
@@ -1,10 +1,16 @@
-import { Field } from './Field';
+import { Field, defaultConfig } from './Field';
 
 describe('Field', () => {
     test('test get', () => {
         const field = new Field;
         const tile = field.getTile(0, 0);
-        console.log(tile);
-        expect(1).toBe(1);
+        expect(tile).toEqual(defaultConfig.defaultTile);
+    });
+
+    test('set', () => {
+        const customTile = { terrain: 'f', blocked: false };
+        const field = new Field({}, { 0: { 0: customTile, 1: { character: 'stuff' } } });
+        expect(field.getTile(0, 0)).toEqual(customTile)
+        expect(field.getTile(0, 1)).toEqual({ ...defaultConfig.defaultTile, character: 'stuff' })
     });
 });
