fccc6b5796e2ca7c0257a6623bcbd88c4ae7c23c
Vincenzo Ciaccio <vincenzo.ciaccio@gmail.com>
2020-04-25 11:09:14 +0100

    small refactory

--- src/components/controls/Actions.js
+++ src/components/controls/Actions.js
@@ -5,6 +5,38 @@ import { Button, Icon } from 'components/common';
 import { ACTIONS_LABELS } from 'downara';
 import { ACTIONS, VIEWS, areaHelper } from 'lib/game';
 
+const SecondaryMenu = ({ showMenu, isPlayerTile, setShowMenu, dispatch }) => {
+    return (
+        <div>
+            <Button
+                className="flex-1"
+                secondary
+                disabled={!isPlayerTile}
+                onClick={() => setShowMenu(!showMenu)}
+            >
+                <Icon name={Icon.names.MENU} />
+            </Button>
+            <div>
+                <Button className="flex-1" onClick={() => dispatch('changeView', VIEWS.INVENTORY)}>
+                    <Icon name={Icon.names.BOX} />
+                </Button>
+                <Button className="flex-1" onClick={() => dispatch('changeView', VIEWS.JOURNAL)}>
+                    <Icon name={Icon.names.BOOK} />
+                </Button>
+                <Button className="flex-1" onClick={() => dispatch('changeView', VIEWS.MAP)}>
+                    <Icon name={Icon.names.MAP} />
+                </Button>
+                <Button className="flex-1" onClick={() => dispatch('changeView', VIEWS.PROFILE)}>
+                    <Icon name={Icon.names.USER} />
+                </Button>
+                <Button className="flex-1" onClick={() => dispatch('changeView', VIEWS.SETTINGS)}>
+                    <Icon name={Icon.names.COG} />
+                </Button>
+            </div>
+        </div>
+    );
+}
+
 const Actions = () => {
     const { dispatch, gameState: { player, actionedTile }, ui: { notification } } = useStoreon('gameState', 'ui');
     const { areaPosition: playerAreaPosition } = player;
@@ -13,35 +45,8 @@ const Actions = () => {
     const [showMenu, setShowMenu] = useState(false);
 
     if (showMenu) {
-        return (
-            <div>
-                <Button
-                    className="flex-1"
-                    secondary
-                    disabled={!isPlayerTile}
-                    onClick={() => setShowMenu(!showMenu)}
-                >
-                    <Icon name={Icon.names.MENU} />
-                </Button>
-                <div>
-                    <Button className="flex-1" onClick={() => dispatch('changeView', VIEWS.INVENTORY)}>
-                        <Icon name={Icon.names.BOX} />
-                    </Button>
-                    <Button className="flex-1" onClick={() => dispatch('changeView', VIEWS.JOURNAL)}>
-                        <Icon name={Icon.names.BOOK} />
-                    </Button>
-                    <Button className="flex-1" onClick={() => dispatch('changeView', VIEWS.MAP)}>
-                        <Icon name={Icon.names.MAP} />
-                    </Button>
-                    <Button className="flex-1" onClick={() => dispatch('changeView', VIEWS.PROFILE)}>
-                        <Icon name={Icon.names.USER} />
-                    </Button>
-                    <Button className="flex-1" onClick={() => dispatch('changeView', VIEWS.SETTINGS)}>
-                        <Icon name={Icon.names.COG} />
-                    </Button>
-                </div>
-            </div>
-        );
+        const props = { showMenu, isPlayerTile, setShowMenu, dispatch }
+        return <SecondaryMenu {...props} />
     }
 
     return (
