9699c7ff2e4c81bcb8d8b44e6162655952b2df63
Vincenzo Ciaccio <vincenzo.ciaccio@gmail.com>
2020-04-25 17:04:57 +0100

    start to work on quests

---
+++ src/downara/quests/index.js
@@ -0,0 +1,8 @@
+
+export default {
+    0: {
+        title: 'Compra il Pane per la Mamma.',
+        description: 'Vai dalla zza Maria e compra il pane per la mamma.',
+        flag: { w: 0, position: { i: 5, j: 4 } }
+    }
+}
