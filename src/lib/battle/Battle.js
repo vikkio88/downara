a390be9d468e1a8ca9c702e92470a2f16b1a23ae
Vincenzo Ciaccio <vincenzo.ciaccio@gmail.com>
2020-04-26 21:55:57 +0100

    implemented turn system

--- src/lib/battle/Battle.js
+++ src/lib/battle/Battle.js
@@ -1,11 +1,21 @@
+const ACTIONS = {
+    MOVE: 'move',
+    ATTACK: 'attack',
+    PARRY: 'parry',
+    USE_ITEM: 'use_item'
+};
+
 export class Battle {
     constructor(field, characters = []) {
         this.characterMap = new Map();
         this.characters = characters;
 
+        this.finished = false;
+
         this.loadCharacters();
         this.turns = this.calculateTurns();
-        this.moves= {};
+        this.needsResolving = false;
+        this.moves = {};
         this.log = {};
     }
 
@@ -14,6 +24,8 @@ export class Battle {
             .sort((c, c1) => c.getSpeed() <= c1.getSpeed() ? 1 : -1)
             .map(c => c.id);
         return {
+            turn: 0,
+            index: 0,
             count: 0,
             order,
             next: order[0],
@@ -26,5 +38,33 @@ export class Battle {
             const character = this.characters[index];
             this.characterMap.set(character.id, character);
         }
+    }
+
+    getCurrentTurn() {
+        return this.turns.next;
+    }
+
+    registerAction(id, type, payload) {
+        const { index, count, order, turn } = this.turns;
+        // registering move
+        const move = this.moves[turn] || [];
+        this.moves[turn] = move;
+        this.moves[turn].push({ id, type, payload });
+
+        // updating turn
+        const nextIndex = (index + 1) % order.length;
+        this.turns.index = nextIndex;
+        this.turns.next = order[nextIndex];
+        this.turns.last = order[index];
+        this.turns.count++;
+
+        if ((count % order.length) === order.length - 1) {
+            this.turns.turn++;
+            this.needsResolving = true;
+        }
+    }
+
+    resolve() {
+
     }
 }
--- src/lib/battle/Battle.test.js
+++ src/lib/battle/Battle.test.js
@@ -2,6 +2,7 @@ import { Battle } from './Battle';
 
 
 describe('Battle test (battletesting battle test)', () => {
+
     describe('initialization', () => {
         it('initializes correctly the battle', () => {
             const characters = [
@@ -11,7 +12,11 @@ describe('Battle test (battletesting battle test)', () => {
 
             const battle = new Battle(null, characters);
             expect(battle.characters).toEqual(characters);
+            expect(battle.finished).toBe(false);
+            expect(battle.needsResolving).toBe(false);
             expect(battle.turns).toEqual({
+                turn: 0,
+                index: 0,
                 count: 0,
                 order: ['test2', 'test1'],
                 next: 'test2',
@@ -22,4 +27,103 @@ describe('Battle test (battletesting battle test)', () => {
             expect(battle.log).toEqual({});
         });
     });
-})
+    describe('turns', () => {
+        it('registering actions', () => {
+            const characters = [
+                { id: 'test1', getSpeed: () => 1 },
+                { id: 'test2', getSpeed: () => 4 },
+            ];
+
+            const battle = new Battle(null, characters);
+            const fakeAction = { type: 'a', payload: { some: 'thing' } };
+            let charId = battle.getCurrentTurn();
+
+            battle.registerAction(charId, fakeAction.type, fakeAction.payload);
+            expect(battle.turns.index).toBe(1);
+            expect(battle.turns.next).toBe('test1');
+            expect(battle.turns.last).toBe(charId);
+            expect(battle.turns.count).toBe(1);
+            expect(battle.turns.turn).toBe(0);
+            expect(battle.moves[0][0]).toEqual({ id: charId, ...fakeAction });
+            expect(battle.needsResolving).toBe(false);
+
+            charId = battle.getCurrentTurn();
+            battle.registerAction(charId, fakeAction.type, fakeAction.payload);
+            expect(battle.turns.index).toBe(0);
+            expect(battle.turns.next).toBe('test2');
+            expect(battle.turns.last).toBe(charId);
+            expect(battle.turns.count).toBe(2);
+            expect(battle.turns.turn).toBe(1);
+            expect(battle.moves[0][1]).toEqual({ id: charId, ...fakeAction });
+            expect(battle.needsResolving).toBe(true);
+        });
+
+        it('registering actions with 3 characters', () => {
+            const characters = [
+                { id: 'test1', getSpeed: () => 1 },
+                { id: 'test2', getSpeed: () => 4 },
+                { id: 'test3', getSpeed: () => 5 },
+            ];
+
+            const battle = new Battle(null, characters);
+            const fakeAction = { type: 'a', payload: { some: 'thing' } };
+            
+            let charId = battle.getCurrentTurn();
+            battle.registerAction(charId, fakeAction.type, fakeAction.payload);
+            expect(battle.turns.index).toBe(1);
+            expect(battle.turns.next).toBe('test2');
+            expect(battle.turns.last).toBe(charId);
+            expect(battle.turns.count).toBe(1);
+            expect(battle.turns.turn).toBe(0);
+            expect(battle.moves[0][0]).toEqual({ id: charId, ...fakeAction });
+            expect(battle.needsResolving).toBe(false);
+
+            charId = battle.getCurrentTurn();
+            battle.registerAction(charId, fakeAction.type, fakeAction.payload);
+            expect(battle.turns.index).toBe(2);
+            expect(battle.turns.next).toBe('test1');
+            expect(battle.turns.last).toBe(charId);
+            expect(battle.turns.count).toBe(2);
+            expect(battle.turns.turn).toBe(0);
+            expect(battle.moves[0][1]).toEqual({ id: charId, ...fakeAction });
+            expect(battle.needsResolving).toBe(false);
+
+            charId = battle.getCurrentTurn();
+            battle.registerAction(charId, fakeAction.type, fakeAction.payload);
+            expect(battle.turns.index).toBe(0);
+            expect(battle.turns.next).toBe('test3');
+            expect(battle.turns.last).toBe(charId);
+            expect(battle.turns.count).toBe(3);
+            expect(battle.turns.turn).toBe(1);
+            expect(battle.moves[0][2]).toEqual({ id: charId, ...fakeAction });
+            expect(battle.needsResolving).toBe(true);
+        });
+
+        it.skip('test', () => {
+            const characters = [
+                { id: 'test1', getSpeed: () => 1 },
+                { id: 'test2', getSpeed: () => 4 },
+                { id: 'test3', getSpeed: () => 2 },
+            ];
+
+            const battle = new Battle(null, characters);
+            const fakeAction = { type: 'a', payload: { some: 'thing' } };
+            for (let i = 0; i <= 30; i++) {
+                let charId = battle.getCurrentTurn();
+                battle.registerAction(charId, fakeAction.type, fakeAction.payload);
+                console.log({
+                    iteration: i,
+                    index: battle.turns.index,
+                    next: battle.turns.next,
+                    last: battle.turns.last,
+                    count: battle.turns.count,
+                    turn: battle.turns.turn
+                });
+            }
+
+            console.log(JSON.stringify(battle.moves));
+
+        });
+    });
+
+});
