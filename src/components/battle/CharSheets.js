1ac900bbd5599190c831f103d5c979ee5d66c904
Vincenzo Ciaccio <vincenzo.ciaccio@gmail.com>
2020-06-11 09:22:11 +0100

    implemented table layout for weapon values

--- src/components/battle/CharSheets.js
+++ src/components/battle/CharSheets.js
@@ -1,26 +1,41 @@
 import React from 'react';
+import get from 'lodash.get';
 import { useStoreon } from 'storeon/react';
 import { CloseRow } from '../common';
 import Stats from './Stats';
 
-const Weapon = ({ name, effects, type, reach, hitDie }) => {
+const ValueRow = ({ label, value }) => {
     return (
-        <>
-            <strong>{name}</strong>
-            <span>
-                <strong>type</strong> {type}
-            </span>
-            <span>
-                <strong>die</strong> {`${hitDie}d20`}
-            </span>
-        </>
+        <tr className="border hover:bg-gray-300 bg-white">
+            <td className="font-semibold border">{label}</td>
+            <td>{value}</td>
+        </tr>
+    );
+};
+
+const Weapon = ({ name, effects, type, reach, hitDie }) => {
+    const damage = get(effects, '0.health.range', null);
+    return (
+        <table className="table-fixed shadow-lg">
+            <thead className="border bg-white">
+                <tr className="text-lg">
+                    <th colSpan="2">{name}</th>
+                </tr>
+            </thead>
+            <tbody>
+                <ValueRow label="type" value={type} />
+                <ValueRow label="die" value={`${hitDie}d20`} />
+                <ValueRow label="reach" value={reach} />
+                <ValueRow label="damage" value={damage ? damage.split(':').join(' - ') : 0} />
+            </tbody>
+        </table>
     );
 };
 
 const CharSheet = ({ char }) => {
     const { dispatch } = useStoreon();
     const isPlayer = !(char.isAi());
-    const header = isPlayer ? <strong>{char.getName()}</strong> : char.getName();
+    const header = <span className={`text-xl${isPlayer ? ' font-semibold' : ''}`}>{char.getName()}</span>;
     const weapon = char.getWeapon().toJs();
     return (
         <div className="flex-1 flex flex-col m-2 bg-gray-300 border-2 border-solid rounded">
