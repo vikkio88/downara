13b18be79fbf8a95a1c2671655f4d25abda2a401
Vincenzo Ciaccio <vincenzo.ciaccio@gmail.com>
2020-06-14 11:27:23 +0100

    moved animation configurations

--- src/phaser/sprites/battle/BattleGrid.js
+++ src/phaser/sprites/battle/BattleGrid.js
@@ -1,102 +1,10 @@
-import { extractFromCoordinates, randomizer, eventBridge } from 'lib';
-import { ACTIONS } from "lib/battle/Battle";
+import { extractFromCoordinates, eventBridge } from 'lib';
 import Tile from './BattleTile';
 import Actor from './Actor';
 import SPRITES from 'downara/sprites';
+import { ACTION_ANIMATIONS, animationFinishedLabel } from './animations';
 import { OBJECT_CONFIG } from '../mapping';
 
-const animationFinishedLabel = index => `turn_${index}_finished`;
-
-const ACTION_ANIMATIONS = {
-    [ACTIONS.MOVE]: (grid, target, result, index) => {
-        const move = ACTIONS.MOVE;
-        if (!result) return ACTION_ANIMATIONS.failedWiggle(grid, target, result, index, move);
-        const { i, j } = result.position || target.getPosition();
-        const tile = grid.getTile({ i, j });
-        const { x, y } = tile.getCenter();
-        const config = {
-            targets: target,
-            x, y,
-            duration: 1000,
-            ease: 'circular.easeInOut',
-            loop: false,
-            onComplete: () => grid.scene.events.emit(animationFinishedLabel(index))
-        };
-        return () => {
-            target.reportResult(move, Boolean(result));
-            grid.scene.tweens.add(config);
-        };
-    },
-    [ACTIONS.ATTACK]: (grid, target, result, index, movePayload) => {
-        const { position } = movePayload;
-        const move = ACTIONS.ATTACK;
-        if (!result) return ACTION_ANIMATIONS.failedTile(grid, target, { position }, index, ACTIONS.ATTACK);
-        const tile = grid.getTile(position);
-        const callback = () => {
-            grid.scene.time.addEvent({
-                delay: 1500,
-                callback: () => {
-                    grid.scene.events.emit(animationFinishedLabel(index));
-                    tile.resetIndicator();
-                },
-                loop: false,
-            });
-        };
-        return () => {
-            target.reportResult(move, Boolean(result));
-            tile.showAttackSuccess(result, callback);
-        };
-    },
-    [ACTIONS.PARRY]: (grid, target, result, index) => {
-        // parry can fail if player died
-        const callback = () => grid.scene.events.emit(animationFinishedLabel(index));
-        if (!result) return () => callback();
-        return () => {
-            target.reportResult(ACTIONS.PARRY, Boolean(result));
-            target.showParry(callback);
-        };
-    },
-    [ACTIONS.USE_ITEM]: () => {
-
-    },
-    failedWiggle: (grid, target, result, index, move) => {
-        const x = randomizer.bool() ? 1 : -1 * randomizer.int(30, 50);
-        const y = randomizer.bool() ? 1 : -1 * randomizer.int(30, 50);
-        const to = randomizer.bool() ? 1 : -1 * randomizer.int(20, 180);
-        return () => {
-            target.reportResult(move);
-            grid.scene.tweens.add({
-                targets: target,
-                x: target.x - x,
-                y: target.y + y,
-                angle: { from: 0, to },
-                duration: 1000,
-                ease: 'sine.out',
-                loop: false,
-                yoyo: true,
-                onComplete: () => grid.scene.events.emit(animationFinishedLabel(index))
-            });
-        };
-    },
-    failedTile: (grid, target, result, index, move) => {
-        const { i, j } = result.position;
-        const tile = grid.getTile({ i, j });
-        const callback = () => {
-            grid.scene.time.addEvent({
-                delay: 1000,
-                callback: () => {
-                    grid.scene.events.emit(animationFinishedLabel(index));
-                    tile.resetIndicator();
-                },
-                loop: false,
-            });
-        };
-        return () => {
-            target.reportResult(move);
-            tile.showFailedMove(callback);
-        };
-    }
-};
 export default class {
     constructor(
         scene,
---
+++ src/phaser/sprites/battle/animations.js
@@ -0,0 +1,95 @@
+import { randomizer } from 'lib';
+import { ACTIONS } from "lib/battle/Battle";
+
+export const animationFinishedLabel = index => `turn_${index}_finished`;
+
+export const ACTION_ANIMATIONS = {
+    [ACTIONS.MOVE]: (grid, target, result, index) => {
+        const move = ACTIONS.MOVE;
+        if (!result) return ACTION_ANIMATIONS.failedWiggle(grid, target, result, index, move);
+        const { i, j } = result.position || target.getPosition();
+        const tile = grid.getTile({ i, j });
+        const { x, y } = tile.getCenter();
+        const config = {
+            targets: target,
+            x, y,
+            duration: 1000,
+            ease: 'circular.easeInOut',
+            loop: false,
+            onComplete: () => grid.scene.events.emit(animationFinishedLabel(index))
+        };
+        return () => {
+            target.reportResult(move, Boolean(result));
+            grid.scene.tweens.add(config);
+        };
+    },
+    [ACTIONS.ATTACK]: (grid, target, result, index, movePayload) => {
+        const { position } = movePayload;
+        const move = ACTIONS.ATTACK;
+        if (!result) return ACTION_ANIMATIONS.failedTile(grid, target, { position }, index, ACTIONS.ATTACK);
+        const tile = grid.getTile(position);
+        const callback = () => {
+            grid.scene.time.addEvent({
+                delay: 1500,
+                callback: () => {
+                    grid.scene.events.emit(animationFinishedLabel(index));
+                    tile.resetIndicator();
+                },
+                loop: false,
+            });
+        };
+        return () => {
+            target.reportResult(move, Boolean(result));
+            tile.showAttackSuccess(result, callback);
+        };
+    },
+    [ACTIONS.PARRY]: (grid, target, result, index) => {
+        // parry can fail if player died
+        const callback = () => grid.scene.events.emit(animationFinishedLabel(index));
+        if (!result) return () => callback();
+        return () => {
+            target.reportResult(ACTIONS.PARRY, Boolean(result));
+            target.showParry(callback);
+        };
+    },
+    [ACTIONS.USE_ITEM]: () => {
+
+    },
+    failedWiggle: (grid, target, result, index, move) => {
+        const x = randomizer.bool() ? 1 : -1 * randomizer.int(30, 50);
+        const y = randomizer.bool() ? 1 : -1 * randomizer.int(30, 50);
+        const to = randomizer.bool() ? 1 : -1 * randomizer.int(20, 180);
+        return () => {
+            target.reportResult(move);
+            grid.scene.tweens.add({
+                targets: target,
+                x: target.x - x,
+                y: target.y + y,
+                angle: { from: 0, to },
+                duration: 1000,
+                ease: 'sine.out',
+                loop: false,
+                yoyo: true,
+                onComplete: () => grid.scene.events.emit(animationFinishedLabel(index))
+            });
+        };
+    },
+    failedTile: (grid, target, result, index, move) => {
+        const { i, j } = result.position;
+        const tile = grid.getTile({ i, j });
+        const callback = () => {
+            grid.scene.time.addEvent({
+                delay: 1000,
+                callback: () => {
+                    grid.scene.events.emit(animationFinishedLabel(index));
+                    tile.resetIndicator();
+                },
+                loop: false,
+            });
+        };
+        return () => {
+            target.reportResult(move);
+            tile.showFailedMove(callback);
+        };
+    }
+};
